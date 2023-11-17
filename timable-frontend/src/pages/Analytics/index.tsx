a952e92a9b9079eeff58fa68153cc775e6ff84e7
biancadenisa <biancadenisa93@yahoo.com>
2023-11-18 00:20:39 +0200

    feat: add type chart

--- timable-frontend/src/pages/Analytics/index.tsx
+++ timable-frontend/src/pages/Analytics/index.tsx
@@ -3,8 +3,13 @@ import "./index.styles.css";
 import DonutChart from "../../components/DonutChart";
 import ColumChart from "../../components/ColumnChart";
 import StaticCards from "../../components/StatisticCards";
+import TypeChart from "../../components/TypeChart";
 
 const Analytics: FC = () => {
+
+  const generatePdf =() => {
+
+  }
   return (
     <>
       <div className="chartContainers">
@@ -12,7 +17,11 @@ const Analytics: FC = () => {
         <ColumChart />
       </div>
 
+      <TypeChart/>
+      <div className='buttonPdfContainer'>
       <StaticCards />
+      <div className="pdfButton" onClick = {() => generatePdf()}>Generate PDF</div>
+      </div>
     </>
   );
 };
