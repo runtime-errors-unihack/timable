29ee21aa9ca1a5614b4400fbbff3e8094c7f3bf2
Anghelescu Mircea <anghelescumircea@yahoo.com>
2023-11-18 00:15:23 +0200

    Add users API and utilities

--- timable-backend/timable_backend/models.py
+++ timable-backend/timable_backend/models.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
-class UserModel(BaseModel):
+class UserBase(BaseModel):
     username: str = Field(description="The user's username")
     email: str = Field(description="The user's email")
     password: str = Field(description="The user's password")
-    is_admin: bool = Field(description="Whether the user is an admin")
+    is_admin: bool = Field(description="Whether the user is an admin", default=False)
+    profile_pic_url: str = Field(description="The user's profile picture location")
+    name: str = Field(description="The user's name")
+    surname: str = Field(description="The user's surname")
+    phone: str = Field(description="The user's phone number")
